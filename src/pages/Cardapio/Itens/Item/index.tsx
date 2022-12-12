8fd5d584361ec494b602db4e5d4d8b22ebacf822
Maria Luiza <malusraimundo@gmail.com>
2022-12-12 08:15:47 -0300

    05. Lista de itens finalizada

--- src/pages/Cardapio/Itens/Item/index.tsx
+++ src/pages/Cardapio/Itens/Item/index.tsx
@@ -1,5 +1,6 @@
 import styles from './Item.module.scss'
 import cardapio from '../itens.json'
+import classNames from 'classnames'
 
 type Props = typeof cardapio[0];
 
@@ -8,7 +9,7 @@ export default function Item(props : Props) {
     return (
         <div className={ styles.item }>
             <div className={ styles.item__imagem }>
-                <img src={ photo } alt="imagem" />
+                <img src={ photo } alt={ title } />
             </div>
             <div className={ styles.tem__descricao }>
                 <div className={ styles.item__titulo }>
@@ -16,17 +17,20 @@ export default function Item(props : Props) {
                     <p>{ description }</p>
                 </div>
                 <div className={ styles.item__tags }>
-                    <div className={ styles.item__tipo }>
+                    <div className={ classNames ({
+                        [styles.item__tipo]: true,
+                        [styles[`item__tipo__${category.label.toLocaleLowerCase()}`]]: true
+                    })}>
                         { category.label }
                     </div>
                     <div className={ styles.item__porcao }>
-                        { size }
+                        { size }g
                     </div>
                     <div className={ styles.item__qtdpessoas }>
-                        { serving }
+                        Serve { serving } pessoa{ serving === 1 ? "" : "s" }
                     </div>
                     <div className={ styles.item__valor }>
-                        R${ price },00
+                        R${ price }
                     </div>
                 </div>
             </div>
